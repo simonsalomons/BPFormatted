45a9a5f212ff6911650bd8121120e9a64ea738a0
Simon Salomons <simon@nextapps.be>
2021-06-14 18:33:37 +0200

    Mark bp methods deprecated when Apple's api is available

--- Sources/BPFormatted/Date Format/Date+Formatted.swift
+++ Sources/BPFormatted/Date Format/Date+Formatted.swift
@@ -9,6 +9,10 @@ import Foundation
 
 extension Date {
 
+    @available(iOS, deprecated: 15.0, message: "Consider using Apple's API", renamed: "formatted")
+    @available(macOS, deprecated: 12.0, message: "Consider using Apple's API", renamed: "formatted")
+    @available(tvOS, deprecated: 15.0, message: "Consider using Apple's API", renamed: "formatted")
+    @available(watchOS, deprecated: 8.0, message: "Consider using Apple's API", renamed: "formatted")
     /// Converts `self` to its textual representation.
     /// - Parameter format: The format for formatting `self`.
     /// - Returns: A representation of `self` using the given `format`. The type of the representation is specified by `FormatStyle.FormatOutput`.
@@ -16,6 +20,10 @@ extension Date {
         format.format(self)
     }
 
+    @available(iOS, deprecated: 15.0, message: "Consider using Apple's API", renamed: "formatted")
+    @available(macOS, deprecated: 12.0, message: "Consider using Apple's API", renamed: "formatted")
+    @available(tvOS, deprecated: 15.0, message: "Consider using Apple's API", renamed: "formatted")
+    @available(watchOS, deprecated: 8.0, message: "Consider using Apple's API", renamed: "formatted")
     /// Converts `self` to its textual representation that contains both the date and time parts. The exact format depends on the user's preferences.
     /// - Parameters:
     ///   - date: The style for describing the date part.
@@ -25,6 +33,10 @@ extension Date {
         bpFormatted(BPFormatStyle(date: date, time: time))
     }
 
+    @available(iOS, deprecated: 15.0, message: "Consider using Apple's API", renamed: "formatted")
+    @available(macOS, deprecated: 12.0, message: "Consider using Apple's API", renamed: "formatted")
+    @available(tvOS, deprecated: 15.0, message: "Consider using Apple's API", renamed: "formatted")
+    @available(watchOS, deprecated: 8.0, message: "Consider using Apple's API", renamed: "formatted")
     public func bpFormatted() -> String {
         bpFormatted(BPFormatStyle())
     }
