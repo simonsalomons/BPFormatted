fb4c5bf2a53ee8f13c1a11a86ec685d280cc5f4e
Simon Salomons <simon@nextapps.be>
2021-06-15 07:26:35 +0200

    Somehow Package.swift got into my gitignore 🤦

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BPFormatted",
+    platforms: [
+        .iOS(.v9),
+        .macOS(.v10_10),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "BPFormatted",
+            targets: ["BPFormatted"]),
+    ],
+    targets: [
+        .target(
+            name: "BPFormatted",
+            dependencies: []),
+        .testTarget(
+            name: "BPFormattedTests",
+            dependencies: ["BPFormatted"]),
+    ]
+)
